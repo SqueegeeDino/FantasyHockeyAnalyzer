0642daddc7961e8474fe2a22c622063a3d25df58
SqueegeeDinoToy <viper970@gmail.com>
2025-09-28 12:42:19 -0700

    Working on test.py NHL API

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-from nhlpy import NHLClient
+from nhlpy.api.query.builder import QueryBuilder, QueryContext
+from nhlpy.nhl_client import NHLClient
+from nhlpy.api.query.filters.draft import DraftQuery
+from nhlpy.api.query.filters.season import SeasonQuery
+from nhlpy.api.query.filters.game_type import GameTypeQuery
+from nhlpy.api.query.filters.position import PositionQuery, PositionTypes
+import json
 
-skaterStats = NHLClient.skater_stats.skater_stats_summary_simple(
-    player_id="8478402", # ID is currently fixed to Connor McDavid
-    start_season='20242025',
-    end_season = '20242025'
-    )
+client = NHLClient()
+
+filters = [
+    SeasonQuery(season_start="20242025", season_end="20242025"),
+    PositionQuery(position=PositionTypes.DEFENSE)
+]
+
+query_builder = QueryBuilder()
+query_context: QueryContext = query_builder.build(filters=filters)
+
+skaterStats = client.stats.skater_stats_with_query_context(
+    report_type='summary',
+    query_context=query_context,
+    aggregate=True
+)
+
+with open("skater_stats.json", "w") as f:
+    json.dump(skaterStats, f, indent=4)  # indent for readability
