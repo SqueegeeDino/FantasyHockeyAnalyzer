8bef4f7115e68037b4bbf513485226e99382f7bf
SqueegeeDinoToy <viper970@gmail.com>
2025-09-29 12:39:10 -0700

    Test.py SQLite3 table initialized
    Manual data entry of the SQLite3 table functional. Next step is to automate this being added from the .json

--- test.py
+++ test.py
@@ -5,25 +5,39 @@ conn = sqlite3.connect('tutorial.db') # Connect to the database. If one doesn't 
 cur = conn.cursor() # Create a cursor. This is used to execute SQL commands and fetch results
 
 cur.execute('''CREATE TABLE IF NOT EXISTS score (
-    name TEXT UNIQUE
-    value INTEGER
+    name TEXT UNIQUE, 
+    value FLOAT,
+    pos TEXT
     )            
-''') # Create the table if there isn't one
+''') # Create the table if there isn't one. We use UNIQUE for the name to prevent duplicating existing entries
 
-# Insert test data into the database
+
 cur.execute("""
-    INSERT INTO movie VALUES 
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)
+    INSERT OR REPLACE INTO score VALUES
+        ('Goals', 4, 'ALL'),
+        ('Assists', 2.5, 'ALL')
 """)
+
 
 conn.commit()
 
-res = cur.execute("SELECT * FROM movie")
+data = [
+    ('Wins', 3, 'G'), # Currently incorrectly registered as 5 to test updating. Set to 3
+    ('Losses', -3, 'G'),
+    ('OTL', 1, 'G'),
+    ('SO', 2.5,  'G'),
+    ('SV', 0.25, 'G'),
+    ('GA', -1, 'G')
+]
+cur.executemany("INSERT OR REPLACE INTO score VALUES(?, ?, ?)", data)
+conn.commit()  # Remember to commit the transaction after executing INSERT.
+
+res = cur.execute("SELECT * FROM score")
 rows = res.fetchall()
 
-print("\n--- Movies in database ---")
+print("\n--- Scores in database ---")
 for row in rows:
     print(row)
 
+# Always close connection
 conn.close()
