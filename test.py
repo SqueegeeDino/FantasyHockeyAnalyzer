8d3817edb7e0cdfb6699412f5fe5626f94cbf2b0
SqueegeeDinoToy <viper970@gmail.com>
2025-09-29 13:16:14 -0700

    Update file output and clean up test script
    Changed output filename from 'league_scoring.json' to 'league_rules.json' in fleakicker-api.py for consistency. Removed redundant print statements and hardcoded database inserts from test.py, streamlining the process of loading scoring rules into the database.

--- test.py
+++ test.py
@@ -16,52 +16,22 @@ with open('league_rules.json', 'r') as file: # Open the 'league_rules.json', in 
     data = json.load(file) # Set the 'data' variable as the file loaded in
 
 for group in data["groups"]: # Search the JSON for the "groups" header and loop through each
-    print(f"\n{group['label']}") # Prints the label of the group, for instance "Goalies"
+    #print(f"\n{group['label']}") # Prints the label of the group, for instance "Goalies"
     if "scoringRules" in group: # Makes sure the group has scoringRules in it
         for rule in group["scoringRules"]: # Loop through the scoringRules
             cat = rule["category"]["abbreviation"] # Get the category abbreviation, for instance "SHP" for Short Handed Points. Side by side items lets us step down without a new loop
             pos = ""
             for app in rule["applyTo"]:
                 pos += app
-            print(pos)
             if rule["forEvery"] == 1: # This checks to see if it's a simple pts per 1 score, or if it's something like "4 pts for every 10"
                 pts = rule["points"]["value"]
-                print("Pts true")
             else: # This is where we check the pt value for a single instance of the event, in case it's a pts per multiple like "4 pts for every 10"
                 pts = rule["pointsPer"]["value"]
-            print(f"  {cat}: {pos} | {pts}") # Print it all nicely
+            #print(f"  {cat}: {pos} | {pts}") # Print it all nicely
             # Add the gathered scoring data to the 'score' table in the 'fleakicker.db'
             d = [cat, pts, pos]
             cur.execute("INSERT OR REPLACE INTO score VALUES(?, ?, ?)", d)
         
-
-
-
-
-
-
-
-
-
-cur.execute("""
-    INSERT OR REPLACE INTO score VALUES
-        ('Goals', 4, 'ALL'),
-        ('Assists', 2.5, 'ALL')
-""")
-
-
-conn.commit()
-
-data = [
-    ('Wins', 3, 'G'), # Currently incorrectly registered as 5 to test updating. Set to 3
-    ('Losses', -3, 'G'),
-    ('OTL', 1, 'G'),
-    ('SO', 2.5,  'G'),
-    ('SV', 0.25, 'G'),
-    ('GA', -1, 'G')
-]
-cur.executemany("INSERT OR REPLACE INTO score VALUES(?, ?, ?)", data)
-conn.commit()  # Remember to commit the transaction after executing INSERT.
 
 res = cur.execute("SELECT * FROM score")
 rows = res.fetchall()
