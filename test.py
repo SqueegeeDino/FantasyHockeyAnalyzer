397fa38b898e779db76f45e01d3ca0b44b906dd7
SqueegeeDinoToy <viper970@gmail.com>
2025-10-09 06:22:12 -0700

    Replace script logic with basic Flask app
    Removed database and API logic, replacing it with a minimal Flask application that serves a simple greeting at the root route.

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-import scoringDatabaseBuilder as sdb
-import requests as rq
-import sqlite3
-import nhlAPI as api
+from flask import Flask
 
-DB_NAME = "fleakicker.db"
-# Run the functions to create tables and populate data
-api.rawstats_dynamic_player()
-api.rawstats_dynamic_goalie()
-sdb.inspect_db_schema(DB_NAME)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, new World!</p>"
