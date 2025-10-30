704f7064d020a99d2f8b63040ab37d14b94b43f6
SqueegeeDino <65366947+SqueegeeDino@users.noreply.github.com>
2025-10-29 20:16:52 -0700

    Found an API that returns hits and blocks for all players

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests as rq
 import json
 
 apiConfig = "https://api.nhle.com/stats/rest/en/config"
-apiTest = "https://api.nhle.com/stats/rest/en/skater/realtime?isAggregate=true&isGame=false&limit=-1&cayenneExp=seasonId=20232024 and gameTypeId=2 and playerId=8480800"
+apiTest = "https://api.nhle.com/stats/rest/en/skater/realtime?isAggregate=true&isGame=false&limit=-1&cayenneExp=seasonId=20252026 and gameTypeId=2"
 
 def dumpAPI(dumpee, filename:str):
     output_filename = filename
